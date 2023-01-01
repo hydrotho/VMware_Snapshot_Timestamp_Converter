96923d19b762ef010b8354ce849e77e9b86bbafa
hydrotho <42911474+hydrotho@users.noreply.github.com>
2023-01-02 00:01:29 +0800

    chore: set `base` for deploying to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/VMware_Snapshot_Timestamp_Converter/',
   plugins: [vue()],
   resolve: {
     alias: {
