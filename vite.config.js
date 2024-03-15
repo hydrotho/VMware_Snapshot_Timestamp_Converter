084393a25279babb98608727ce9e013dc1211f7e
hydrotho <42911474+hydrotho@users.noreply.github.com>
2024-03-15 23:02:45 +0800

    chore: vite plugin VueDevtools

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,21 @@
-import { resolve, dirname } from 'node:path'
-import { fileURLToPath, URL } from 'node:url'
+import { dirname, resolve } from 'node:path'
+import { URL, fileURLToPath } from 'node:url'
 
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
 
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
+import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/VMware_Snapshot_Timestamp_Converter/',
   plugins: [
     vue(),
+    VueDevTools(),
     VueI18nPlugin({
-      include: resolve(dirname(fileURLToPath(import.meta.url)), './src/i18n/locales/**'),
-    }),
+      include: resolve(dirname(fileURLToPath(import.meta.url)), './src/i18n/locales/**')
+    })
   ],
   resolve: {
     alias: {
